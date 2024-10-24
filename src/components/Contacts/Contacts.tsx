6d23f60163561a22ad69270a6b67bb76c6283077
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-24 20:22:45 +0600

    Создал компоненты About и Contacts и проложил маршрут

---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,30 @@
+const Contacts = () => {
+
+    const style = {
+        padding: "40px",
+        color: "white",
+        fontSize: "26px",
+    };
+
+    return (
+        <div style={{background: "black"}}>
+            <h2 style={style}>
+                SPIDER-MAN™: NO WAY HOME Now on Digital | On 4K Ultra HD™, Blu-ray™ and DVD April 12
+                © 2022 Layout and Design Sony Pictures Home Entertainment Inc. All Rights Reserved
+            </h2>
+            <h2 style={style}>
+                © 2022 SONY PICTURES DIGITAL PRODUCTION INC. ALL RIGHTS RESERVED.
+                MARVEL AND ALL RELATED CHARACTER NAMES: © & ™ 2022 MARVEL
+                Footer
+            </h2>
+            <h2 style={style}>
+
+                For Ratings Reasons: Filmratings.com | MPA | Privacy Policy | CA Privacy Policy | Children's Privacy
+                Policy | AD Choices | Terms of Use | Your Privacy Choices | Cookie Consent Tool
+                US Consumer DSR
+            </h2 >
+        </div>
+    );
+};
+
+export default Contacts;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,8 +6,8 @@ const Navbar = () => {
     return (
         <>
             <Box sx={{ flexGrow: 1 , mb: 4}}>
-                <AppBar position="static">
-                    <Toolbar>
+                <AppBar position="static" sx={{background: "red"}}>
+                    <Toolbar sx={{ background: 'red', width: 1200, mx: 'auto' }}>
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             My blog
                         </Typography>
