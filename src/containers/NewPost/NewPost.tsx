0695e6564e24903df076d6f401a9f781eca145c4
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-24 19:40:55 +0600

    Вызов формы для добавления поста вывел в отдельный компонент

---
+++ src/containers/NewPost/NewPost.tsx
@@ -0,0 +1,18 @@
+import {IPostForm} from "../../types";
+import axiosAPI from "../../axiosAPI.tsx";
+import PostForm from "../../components/PostForm/PostForm.tsx";
+
+const NewPost = () => {
+
+    const submitForm = async (post: IPostForm) => {
+        await axiosAPI.post('post.json', {...post, date: new Date().toISOString()});
+    };
+
+    return (
+        <>
+         <PostForm submitForm={submitForm} />
+        </>
+    );
+};
+
+export default NewPost;
